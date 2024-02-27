d6b30279231265e78a15f7e7cf84523b4e4d0de4
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-02-26 23:04:52 -0800

    function select() partially completed.

--- Lab4_func.cpp
+++ Lab4_func.cpp
@@ -7,10 +7,19 @@ int sumOfThree(int x, int y, int z){
 }
 
 double division(double x, double y){
-    double divide = x / y;
-    return divide;
+    double division = x / y;
+    return division;
 }
 
 bool isGreaterThan(double x, int y){
-    return x > y;
+    bool compare = x > y;
+    return compare;
+}
+
+string select(string func_type){
+    string out;
+
+    if (func_type == "sum"){
+
+    }
 }
